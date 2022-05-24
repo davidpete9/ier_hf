c2110a84df46a1127dd35a22229ecdc47c2c87e3
David Pete <dervid9@gmail.com>
2022-05-24 17:23:18 +0200

    refact calc cost

--- ier_hf/calc/CalculateEnergyLoss.java
+++ ier_hf/calc/CalculateEnergyLoss.java
@@ -3,10 +3,15 @@ package calc;
 public class CalculateEnergyLoss {
 
   public static final int DEFAULT_ENERGY = 1;
+  public static final int DEFAULT_CHARGE_SPEED = 1;
   public static final int DEFAULT_MULTIPLIER = 10;
   
   public static int calculateEnergyPerTime(int capacity, int weight) {
-  	return (int)(((double)weight/capacity)*DEFAULT_MULTIPLIER+DEFAULT_ENERGY)
+  	return (int)(((double)weight/capacity)*DEFAULT_MULTIPLIER+DEFAULT_ENERGY);
+  }
+  
+  public static int getChargeTime(int requiredCapacity) {
+  	return requiredCapacity*DEFAULT_CHARGE_SPEED;
   }
   
   public CalculateEnergyLoss() {}
--- ier_hf/calc/calc_cost.java
+++ ier_hf/calc/calc_cost.java
@@ -43,82 +43,151 @@ public class calc_cost extends DefaultInternalAction {
 
         return ret;
     }
+    
+    public int timeCost(int dist, int speed) {
+    	return (int)Math.ceil(dist/speed);
+    }
+    
+    
 
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] terms) throws Exception {
         int maxBid = 9999999; // The agent bids an unrealistically high bid, if it cannot do this delivery
 
-        int posX = (int) ((NumberTerm) terms[0]).solve(); // Drone pos x
-        int posY = (int) ((NumberTerm) terms[1]).solve(); // Drone pos y
+	// The last planned position of the drone
+        int lastX = (int) ((NumberTerm) terms[0]).solve(); 
+        int lastY = (int) ((NumberTerm) terms[1]).solve(); 
 
-        int goalX = (int) ((NumberTerm) terms[2]).solve(); // Goal x
-        int goalY = (int) ((NumberTerm) terms[3]).solve(); // Goal y
+	// Order parameters
+        int goalX = (int) ((NumberTerm) terms[2]).solve();
+        int goalY = (int) ((NumberTerm) terms[3]).solve();
+        int itemWeight = (int) ((NumberTerm) terms[4]).solve(); // Weight of the item
+        
+       
+        //Drone parameters
+        int cargoCapacity = (int) ((NumberTerm) terms[5]).solve(); // The heaviest object the drone can carry
+        int speed = (int) ((NumberTerm) terms[6]).solve(); 
+        int chargeWillBe = (int) ((NumberTerm) terms[7]).solve(); // the battery level of the drone, predicted at the time when this order can be completed.
+        
+        int baseTime = (int) ((NumberTerm) terms[8]).solve(); // The time required to finish the remaining tasks.
+        
+        
 
-        int cargoCapacity = (int) ((NumberTerm) terms[4]).solve(); // The heaviest object the drone can carry
-        int itemWeight = (int) ((NumberTerm) terms[5]).solve(); // Weight of the item
-        int charge = (int) ((NumberTerm) terms[6]).solve(); // 0-100% - Charge of the drone's battery
-        int speed = (int) ((NumberTerm) terms[7]).solve(); // Speed of the drone
-
-        double energyConsumption = (itemWeight/cargoCapacity) + 0.314; // For one unit of distance
-
+	// Drone has this information in it's belief base, but it is simpler to query this way.
         WorldModel model = WorldModel.get();
         ArrayList<Location> refuelStations = new ArrayList<Location>();
         refuelStations.addAll(model.getDepots());
-        refuelStations.add(model.getMainDepot());
-
-        Location closestStationToDestination = get_closest_station(goalX, goalY, refuelStations);
+        refuelStations.add(model.getMainDepot()); 
         
-        //distance from Goal To Station
-        int distFromStation = get_dist(goalX, goalY, closestStationToDestination.x, closestStationToDestination.y);
-
-	//From position to Goal
-        int dist = get_dist(posX, posY, goalX, goalY);
-
-        double cost = (dist + distFromStation) / (double) speed; // The bid/cost is the distance it takes for the drone to complete the delivery times the speed
-
-
-	int chargeRequired = (int)Math.round((dist + distFromStation) * energyConsumption);
-        int chargeLeft = charge - chargeRequired; 
-
-        int actualCost = maxBid;
+        Location main = model.getMainDepot();
         
-        int chargeAtX = 0;
-        int chargeAtY = 0;
-        double chargeT = 0;
-    		
-    	double chargePerTValue = 0.5;
-    	    
-        if (chargeLeft > 0) {
-            actualCost = (int) Math.round(cost * 100);
+        //Cost calculation:
+        //  Option 1:
+        //  1. Path cost to mainDepot (no cargo) (if not there)
+        //  2. Path from mainDepot to destination 
+        //  3. From destionation go back to main depot. (or )
+        
+        
+        
+        int to_maindepot = timeCost(get_dist(lastX,lastY, main.x,main.y),speed);  
+        
+        int from_depot_to_goal = timeCost(get_dist(main.x, main.y, goalX, goalY),speed); 
+        
+        int from_goal_back_to_main_depot = timeCost(get_dist(goalX,goalY,main.x,main.y),speed);
+        
+        
+        int costWithCargo = CalculateEnergyLoss.calculateEnergyPerTime(cargoCapacity,itemWeight);
+        int costWithNoCargo = CalculateEnergyLoss.calculateEnergyPerTime(cargoCapacity,0);
+        
+        int chargeCost = (to_maindepot+from_goal_back_to_main_depot)*costWithNoCargo+from_depot_to_goal*costWithCargo;
+        
+        
+        int chargeT = 0;
+        int full_cost = baseTime+to_maindepot+from_depot_to_goal;
+        
+       
+        
+        if (chargeWillBe - chargeCost > 0) { //1. OK. No need to charge, it can go back to main AFTER the order!
+        	        
+        	         un.unifies(terms[9], new NumberTermImpl(main.x));//ReturnAtX
+	                 un.unifies(terms[10], new NumberTermImpl(main.y)); //ReturnAtY
+			 un.unifies(terms[11], new NumberTermImpl(full_cost));
+			 un.unifies(terms[12], new NumberTermImpl(chargeWillBe-chargeCost));
+			 un.unifies(terms[13], new NumberTermImpl(main.x)); //StartAtX
+			 un.unifies(terms[14], new NumberTermImpl(main.y)); //StartAtY
+       	 return un.unifies(terms[15], new NumberTermImpl(0));
+           
         }
-        else { 
-        //Charge first. Get the closest station from the actual position
-        Location closestStationFromPos = get_closest_station(posX, posY, refuelStations);
-        int distToStation = get_dist(posX, posY, closestStationFromPos.x, closestStationFromPos.y);
-        int chargeLeftAtStation = charge - (int) Math.round(distToStation * energyConsumption);
-        if (chargeLeftAtStation > 0) {
-        	chargeAtX = closestStationFromPos.x;
-        	chargeAtY = closestStationFromPos.y;
-        	chargeT = (chargeRequired-chargeLeftAtStation)*chargePerTValue;
-        	chargeLeft = 0;
-        }
+        else {
+           Location closestStationToDestination = get_closest_station(goalX, goalY, refuelStations);
+           int from_goal_to_closest_depo = timeCost(get_dist(goalX,goalY,closestStationToDestination.x,closestStationToDestination.y),speed);
+           
+           int chargeCostToClosest = (to_maindepot+from_goal_to_closest_depo)*costWithNoCargo+from_depot_to_goal*costWithCargo;
+           
+           if (chargeWillBe - chargeCostToClosest > 0 ) { //2. Ok. No need to charge, but it goes to a refuel depot insted of the main AFTER it completes the order.
+           
+           	         un.unifies(terms[9], new NumberTermImpl(closestStationToDestination.x));
+			 un.unifies(terms[10], new NumberTermImpl(closestStationToDestination.y));
+			 un.unifies(terms[11], new NumberTermImpl(full_cost));
+			 un.unifies(terms[12], new NumberTermImpl(chargeWillBe-chargeCostToClosest));
+			 un.unifies(terms[13], new NumberTermImpl(main.x));
+			 un.unifies(terms[14], new NumberTermImpl(main.y));
+       	 return un.unifies(terms[15], new NumberTermImpl(0));
+           
+           }
+           else { // It must charge before! 
+           	chargeCost = to_maindepot*costWithNoCargo;
+           	if (chargeWillBe - chargeCost > 0) { // 3. It can go to main depot to charge first.
+           	     //Charge cost need to be enught to after the delivery it should be enought to go to the closest depot.
+           	         chargeT = CalculateEnergyLoss.getChargeTime(chargeCostToClosest-(chargeWillBe-chargeCost));
+           	         un.unifies(terms[9], new NumberTermImpl(closestStationToDestination.x));//ReturnAtX
+	                 un.unifies(terms[10], new NumberTermImpl(closestStationToDestination.y)); //ReturnAtY
+           	     	 un.unifies(terms[11], new NumberTermImpl(full_cost+chargeT));
+			 un.unifies(terms[12], new NumberTermImpl(0));
+			 un.unifies(terms[13], new NumberTermImpl(main.x));
+			 un.unifies(terms[14], new NumberTermImpl(main.y)); //StartAtY
+       	    return un.unifies(terms[15], new NumberTermImpl(chargeT));
+           	     
+           	}
+           	else { //Cant go to main depo with this charge level, but may be can to a depot.
+           	   Location closestDepot = get_closest_station(lastX, lastY, refuelStations);
+           	   int to_closest_depot = timeCost(get_dist(lastX,lastY,closestDepot.x,closestDepot.y),speed);
+           	   chargeCost = to_closest_depot*costWithNoCargo;
+           	   if (chargeWillBe - chargeCost > 0) {
+           	   	//Charge cost: charge need to be enought to go to main depot, and than deliver the package, and than go back to a closest station.
+           	   	int from_depot_to_main = timeCost(get_dist(closestDepot.x,closestDepot.y,main.x,main.y),speed);
+           	   	int chargeToMain = from_depot_to_main*costWithNoCargo;
+           	   	int fromMainToGoalAndClosest = from_depot_to_goal*costWithCargo+from_goal_to_closest_depo*costWithNoCargo;
+           	   	
+           	   	chargeT = CalculateEnergyLoss.getChargeTime((chargeCost+chargeToMain+fromMainToGoalAndClosest)-chargeWillBe);
+           	   	
+           	   	un.unifies(terms[9], new NumberTermImpl(closestStationToDestination.x));//ReturnAtX
+	                un.unifies(terms[10], new NumberTermImpl(closestStationToDestination.y)); //ReturnAtY
+           	     	un.unifies(terms[11], new NumberTermImpl(full_cost+chargeT+to_closest_depot+from_depot_to_main));
+			un.unifies(terms[12], new NumberTermImpl(0));
+			un.unifies(terms[13], new NumberTermImpl(closestDepot.x)); //StartAtX = ChargeAtX
+			un.unifies(terms[14], new NumberTermImpl(closestDepot.y)); //StartAtY
+       	        return un.unifies(terms[15], new NumberTermImpl(chargeT));
+           	   }
+           	   else {
+           	      ts.getLogger().info("A dron lezuhan, nincs eleg toltottsege sehova :((");
+           	      return true;
+           	   }
+           	}
+           
+           }	
+		        
         }
 
-        if (printinfo) {
+
+      /*  if (printinfo) {
             ts.getLogger().info("Closest station " + closestStationToDestination.x + " " + closestStationToDestination.y);
             ts.getLogger().info("Distance fromt there " + distFromStation);
             ts.getLogger().info("Distance to goal: " + dist);
             ts.getLogger().info("Calculated cost: " + cost + " all dist: " + (dist + distFromStation) + " and speed: " + speed);
             ts.getLogger().info("Charge left: " + chargeLeft);
-        }
+        }*/
 
-        un.unifies(terms[8], new NumberTermImpl(closestStationToDestination.x));
-        un.unifies(terms[9], new NumberTermImpl(closestStationToDestination.y));
-        un.unifies(terms[10], new NumberTermImpl(actualCost));
-        un.unifies(terms[11], new NumberTermImpl(chargeLeft));
-        un.unifies(terms[13], new NumberTermImpl(chargeAtX));
-        un.unifies(terms[13], new NumberTermImpl(chargeAtY));
-        return un.unifies(terms[14], new NumberTermImpl(chargeT));
         
      	//return true;
     }
